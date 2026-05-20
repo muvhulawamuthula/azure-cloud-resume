579833e86cb82d893fe1ef1704f252fc3ef756f1
muvhulawamuthula <muthulamuvhulawavhofhololani@gmail.com>
2026-05-20 06:40:18 +0200

    Persist visitor counter in Azure Table Storage

--- api/src/functions/visitorCounter.js
+++ api/src/functions/visitorCounter.js
@@ -1,18 +1,50 @@
 const { app } = require("@azure/functions");
+const { TableClient, AzureNamedKeyCredential } = require("@azure/data-tables");
 
-let visitorCount = 0;
+const accountName = process.env.STORAGE_ACCOUNT_NAME;
+const accountKey = process.env.STORAGE_ACCOUNT_KEY;
+const tableName = process.env.TABLE_NAME || "VisitorCounter";
+
+const credential = new AzureNamedKeyCredential(accountName, accountKey);
+const client = new TableClient(
+  `https://${accountName}.table.core.windows.net`,
+  tableName,
+  credential
+);
 
 app.http("visitorCounter", {
   methods: ["GET"],
   authLevel: "anonymous",
-  handler: async (request, context) => {
-    visitorCount++;
+  handler: async () => {
+    const partitionKey = "portfolio";
+    const rowKey = "visitor-count";
+
+    let entity;
+
+    try {
+      entity = await client.getEntity(partitionKey, rowKey);
+      entity.count = Number(entity.count || 0) + 1;
+
+      await client.updateEntity(entity, "Merge");
+    } catch (error) {
+      if (error.statusCode === 404) {
+        entity = {
+          partitionKey,
+          rowKey,
+          count: 1
+        };
+
+        await client.createEntity(entity);
+      } else {
+        throw error;
+      }
+    }
 
     return {
       status: 200,
       jsonBody: {
-        count: visitorCount,
-        message: "Visitor counter is working"
+        count: entity.count,
+        message: "Persistent visitor counter is working"
       }
     };
   }
